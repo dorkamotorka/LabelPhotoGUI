fd8dee6df5f808c074e751796d1282183ff7a4c3
Teo <tp4348@student.uni-lj.si>
2020-05-31 18:24:18 +0200

    Stream working.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
 import cv2 as cv
-import torch
-import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
 import os
 
-print(torch.cuda.is_available())
+cap = cv.VideoCapture(0)
 
-cam = cv.VideoCapture(0)
-disp = tk.Tk()
-butt = tk.Button(disp, text="Obstacle") 
+root = Tk()
+#root.bind('<Key>', lambda e: root.quit())
+lmain = Label(root)
+lmain.pack()
+
+def process_frame():
+    ret, frame = cap.read()
+    if ret:
+        img = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)
+        img = Image.fromarray(img)
+        imgtk = ImageTk.PhotoImage(image=img) 
+        lmain.imgtk = imgtk
+        lmain.configure(image=imgtk)
+        lmain.after(1, process_frame)
+
+
+def labelPhoto():
+    pass
+
+
+
+process_frame()
+root.mainloop()
+
 
