c3270ac110c349efcfc15873edf3c97d4651e1b1
Teo <tp4348@student.uni-lj.si>
2020-05-31 23:36:50 +0200

    Added buttons.

--- src/main.py
+++ src/main.py
@@ -3,10 +3,21 @@ from tkinter import *
 from PIL import Image, ImageTk
 import os
 
+
+def ObstacleLabel():
+    print('obstacle!')
+
+def FreepathLabel():
+    print('freepath!')
+
 cap = cv.VideoCapture(0)
 
 root = Tk()
-#root.bind('<Key>', lambda e: root.quit())
+btn = Button(root, text='Obstacle', bd='5', bg='blue', fg='white', command=ObstacleLabel) 
+btn.pack(side='bottom')
+btn = Button(root, text='Free-path', bd='5', bg='red', fg='white', command=FreepathLabel)
+btn.pack(side='bottom', padx='5', pady='5')
+#root.bind('<Key>', lambda e: root.destroy())
 lmain = Label(root)
 lmain.pack()
 
@@ -17,13 +28,8 @@ def process_frame():
         img = Image.fromarray(img)
         imgtk = ImageTk.PhotoImage(image=img) 
         lmain.imgtk = imgtk
-        lmain.configure(image=imgtk)
+        lmain.configure(image=imgtk, background='black')
         lmain.after(1, process_frame)
-
-
-def labelPhoto():
-    pass
-
 
 
 process_frame()
